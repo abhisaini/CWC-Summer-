f39761e76c2a350ccadcd6812d76d739f8956ed0
Abhi Saini <abhisni@iitk.ac.in>
2018-05-05 10:01:11 +0530

    fix

--- summer_camp/views.py
+++ summer_camp/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import redirect, render
 def model_form_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+        # print(form.cleaned_data['name'])
         if form.is_valid():
+            sub_name = form.cleaned_data['name']
+            file_name = request.FILES['document'].name
             form.save()
-            return redirect('/home')
+            return render(request, 'successful.html',{'name': sub_name, 'filename': file_name})
     else:
         form = DocumentForm()
     return render(request, 'assignment_form.html', {'form': form})
